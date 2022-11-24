4a0b899817c4e7af8228a582ccce4f3b29ca789b
Osama Iqbal <osama.iqbal@Powersoft19.com>
2022-11-24 21:13:56 +0500

    server endpoint

--- frontend/src/utils/serverEndpoint.js
+++ frontend/src/utils/serverEndpoint.js
@@ -1,12 +1,6 @@
 export function getServerEndpoint() {
-
-  return window.location.protocol + '//' + getServerEndpointNoProtocol();
-
-}
-
-export function getServerEndpointNoProtocol() {
-  let serverEndpoint = "localhost:4001/"; /*'localhost:3005/'*/ /* 'iahmed:3005/' */ /*"172.19.91.147:4001/"*/
-  console.log(process.env.NODE_ENV);
+  let serverEndpoint = "localhost:4001/";  /*'localhost:3005/'*/ /* 'iahmed:3005/' */ /*"172.19.91.147:4001/"*/
+  console.log(process.env.NODE_ENV)
   if (process.env.NODE_ENV === "production") {
     serverEndpoint = window.location.host + "/"; //window.location.protocol + '//'+ window.location.host +'/';
   }
