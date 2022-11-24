38cafbfd82f1ba90f794235abba524fbe693235e
Osama Iqbal <osama.iqbal@Powersoft19.com>
2022-11-24 20:06:18 +0500

    unifiedtopology

--- frontend/src/utils/serverEndpoint.js
+++ frontend/src/utils/serverEndpoint.js
@@ -1,5 +1,11 @@
 export function getServerEndpoint() {
-  let serverEndpoint = "localhost:10000/";  /*'localhost:3005/'*/ /* 'iahmed:3005/' */ /*"172.19.91.147:4001/"*/
+
+  return "https:" + '//' + getServerEndpointNoProtocol();
+
+}
+
+export function getServerEndpointNoProtocol() {
+  let serverEndpoint = "localhost:4001/"; /*'localhost:3005/'*/ /* 'iahmed:3005/' */ /*"172.19.91.147:4001/"*/
   console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV === "production") {
     serverEndpoint = window.location.host + "/"; //window.location.protocol + '//'+ window.location.host +'/';
